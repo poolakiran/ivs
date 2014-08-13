c350f38f87000ca410b67558482b289e790f338c
Rich Lane <rlane@bigswitch.com>
2014-08-13 12:42:21 -0700

    inband: generic LLDP parsing
    Adds a packet-in listener and iterates over each TLV in received LLDPs.

--- modules/inband/module/src/inband.c
+++ modules/inband/module/src/inband.c
@@ -18,12 +18,122 @@
  ****************************************************************/
 
 #include <AIM/aim.h>
+#include <stdbool.h>
 #include "inband_int.h"
 #include "inband_log.h"
+#include <indigo/of_state_manager.h>
+#include <PPE/ppe.h>
+
+struct lldp_tlv {
+    uint8_t type;
+    uint32_t oui;
+    uint8_t subtype;
+    const uint8_t *payload;
+    uint16_t payload_length;
+};
+
+/*
+ * Parse the LLDP TLV starting at *data_p. Returns true if parsing was
+ * successful. *remain should be initialized with the number of bytes
+ * available starting from *data_p. After each successful call this
+ * function will update *data_p, *remain, and *tlv.
+ */
+static bool
+lldp_parse_tlv(const uint8_t **data_p, int *remain, struct lldp_tlv *tlv)
+{
+    const uint8_t *data = *data_p;
+
+    if (*remain < 2) {
+        AIM_LOG_WARN("Not enough bytes remaining for an LLDP TLV");
+        return false;
+    }
+
+    memset(tlv, 0, sizeof(*tlv));
+    tlv->type = data[0] >> 1;
+    int payload_length = ((data[0] & 1) << 8) | data[1];
+
+    if (tlv->type == 0 && payload_length == 0) {
+        /* End of LLDPDU */
+        return false;
+    }
+
+    int total_length = payload_length + 2;
+    if (total_length > *remain) {
+        AIM_LOG_WARN("Invalid LLDP TLV length %d", total_length);
+        return false;
+    }
+
+    tlv->payload = data + 2;
+    tlv->payload_length = payload_length;
+
+    if (tlv->type == 127) {
+        if (payload_length < 4) {
+            AIM_LOG_WARN("Not enough payload bytes for an LLDP organizational TLV");
+            return false;
+        }
+        tlv->oui = (data[2] << 16) | (data[3] << 8) | data[4];
+        tlv->subtype = data[5];
+        tlv->payload += 4;
+        tlv->payload_length -= 4;
+    }
+
+    *data_p += total_length;
+    *remain -= total_length;
+    assert(*remain >= 0);
+
+    return true;
+}
+
+static indigo_core_listener_result_t
+pktin_listener(of_packet_in_t *packet_in)
+{
+    if (packet_in->version != OF_VERSION_1_3) {
+        return INDIGO_CORE_LISTENER_RESULT_PASS;
+    }
+
+    of_match_t match;
+    if (of_packet_in_match_get(packet_in, &match) < 0) {
+        AIM_LOG_ERROR("Failed to parse packet-in match");
+        return INDIGO_CORE_LISTENER_RESULT_PASS;
+    }
+
+    /* TODO Check if the source port is an uplink */
+
+    of_octets_t octets;
+    of_packet_in_data_get(packet_in, &octets);
+
+    /*
+     * Identify if this is an LLDP Packet
+     */
+    ppe_packet_t ppep;
+    ppe_packet_init(&ppep, octets.data, octets.bytes);
+    if (ppe_parse(&ppep) < 0) {
+        AIM_LOG_WARN("Packet-in parsing failed");
+        return INDIGO_CORE_LISTENER_RESULT_PASS;
+    }
+
+    uint8_t *header;
+    if ((header = ppe_header_get(&ppep, PPE_HEADER_LLDP)) == NULL) {
+        AIM_LOG_VERBOSE("Not an LLDP packet");
+        return INDIGO_CORE_LISTENER_RESULT_PASS;
+    }
+
+    AIM_LOG_VERBOSE("Parsing LLDP packet");
+
+    struct lldp_tlv tlv;
+    int remain = octets.bytes - (header - octets.data);
+    const uint8_t *pos = header;
+    while (lldp_parse_tlv(&pos, &remain, &tlv)) {
+        AIM_LOG_TRACE("Found tlv type=%u oui=%u subtype=%u payload_length=%u", tlv.type, tlv.oui, tlv.subtype, tlv.payload_length);
+    }
+
+    return INDIGO_CORE_LISTENER_RESULT_PASS;
+}
 
 void
 inband_init(void)
 {
+    (void) indigo_core_packet_in_listener_register(pktin_listener);
 }
 
 void
--- modules/inband/utest/main.c
+++ modules/inband/utest/main.c
@@ -25,6 +25,52 @@
 #include <arpa/inet.h>
 #include <AIM/aim.h>
 #include <loci/loci.h>
+#include <indigo/of_state_manager.h>
+
+static uint8_t lldp1[] = {
+    // Destination MAC
+    0x01, 0x80, 0xc2, 0x00, 0x00, 0x0e,
+    // Source MAC
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    // LLDP Ether Type
+    0x88, 0xcc,
+    // Chassis ID
+    0x02, 0x07, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    // Port ID
+    0x04, 0x04, 0x05, 0x61, 0x6c, 0x6c,
+    // TTL
+    0x06, 0x02, 0x00, 0x78,
+    // System Name
+    0x0a, 0x0a, 0x50, 0x4F, 0x44, 0x2D, 0x53, 0x65, 0x6E, 0x64, 0x65, 0x72,
+    // System Desc
+    0x0c, 0x11, 0x35, 0x63, 0x3A, 0x31, 0x36, 0x3A, 0x63, 0x37, 0x3A, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x3A, 0x30, 0x31,
+    // Switch Type
+    0xfe, 0x05, 0x00, 0x26, 0xe1, 0x01, 0x01,
+    // Direction TLV
+    0xfe, 0x05, 0x00, 0x26, 0xe1, 0x03, 0x01,
+    // Controller ID TLV
+    0xfe, 0x10, 0x00, 0x26, 0xe1, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+    // End of LLDP PDU
+    0x00, 0x00
+};
+
+static indigo_core_packet_in_listener_f listener = NULL;
+
+static void
+packet_in(const uint8_t *data, int length, of_port_no_t in_port)
+{
+    of_packet_in_t *obj = of_packet_in_new(OF_VERSION_1_3);
+    of_match_t match = { 0 };
+    match.version = OF_VERSION_1_3;
+    match.fields.in_port = in_port;
+    OF_MATCH_MASK_IN_PORT_EXACT_SET(&match);
+    AIM_TRUE_OR_DIE(of_packet_in_match_set(obj, &match) == 0);
+    of_octets_t octets = { .data = (uint8_t *)data, .bytes = length };
+    AIM_TRUE_OR_DIE(of_packet_in_data_set(obj, &octets) == 0);
+    indigo_core_listener_result_t result = listener(obj);
+    AIM_ASSERT(result == INDIGO_CORE_LISTENER_RESULT_PASS);
+    of_object_delete(obj);
+}
 
 int aim_main(int argc, char* argv[])
 {
@@ -35,5 +81,17 @@ int aim_main(int argc, char* argv[])
 
     assert(listener != NULL);
 
+    packet_in(lldp1, sizeof(lldp1), 1);
+
     return 0;
+}
+
+/* OFStateManager stubs */
+
+indigo_error_t
+indigo_core_packet_in_listener_register(indigo_core_packet_in_listener_f fn)
+{
+    assert(listener == NULL);
+    listener = fn;
+    return INDIGO_ERROR_NONE;
 }
